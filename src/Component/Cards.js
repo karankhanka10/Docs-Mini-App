62fc4b1b400b1414c1b19790b959c859b87e8461
Karan Khanka <karankhanka@icloud.com>
2024-08-14 02:28:53 +0530

    completed

--- src/Component/Cards.js
+++ src/Component/Cards.js
@@ -2,21 +2,20 @@ import React from 'react';
 import DescriptionIcon from '@mui/icons-material/Description';
 import DownloadForOfflineIcon from '@mui/icons-material/DownloadForOffline';
 import Slider  from './Slider';
+import { motion } from "framer-motion"
 
-const Cards = () => {
-  const showFooter = false;
-  const isColor = true;
-  const isTag = true;
+const Cards = (props) => {
+ 
 
   return (
-    <div className='relative w-40 h-56 bg-zinc-900/90 rounded-[30px] top-16 left-7 p-4 overflow-hidden'>
+    <motion.div drag dragConstraints={props.reference} whileDrag={{scale:1.2}} className='relative w-40 h-56 bg-zinc-900/90 rounded-[30px] top-16 left-7 p-4 overflow-hidden'>
       <DescriptionIcon className='absolute text-white text-3xl top-5 left-3' />
       <h1 className='text-white text-[13px] mt-10'>
-        После установки пакетов вы можете импортировать значки, которые хотите
+        После установки пакетов вы можете импортировать значки, которые 
       </h1>
       <div className='absolute bottom-0 w-full left-0'>
-        {showFooter ? (
-          <div className={`flex justify-between items-center bg-zinc-900/90 pr-7 pb-6 pl-4 ${isColor ? 'text-white' : 'text-white'}`}>
+        {props.showFooter ? (
+          <div className={`flex justify-between items-center bg-zinc-900/90 pr-7 pb-6 pl-4 ${props.isColor ? 'text-white' : 'text-white'}`}>
             <h1 className='text-[14px] '>
               5mb
             </h1>
@@ -24,21 +23,21 @@ const Cards = () => {
           </div>
         ) : (
           <>
-            <div className={`flex justify-between items-center bg-zinc-900/90 pr-7 pb-6 pl-4 ${isColor ? 'text-green-500' : 'text-blue-500'}`}>
-            <h1 className={`text-[14px] ${isColor ? 'text-green-500' : 'text-blue-500'}`}>
+            <div className={`flex justify-between items-center bg-zinc-900/90 pr-7 pb-6 pl-4 ${props.isColor ? 'text-green-500' : 'text-blue-500'}`}>
+            <h1 className={`text-[14px] ${props.isColor ? 'text-green-500' : 'text-blue-500'}`}>
               5mb
             </h1>
               <DownloadForOfflineIcon className='text-white'/>
             </div>
-            <div className={`h-9 ${isTag ? 'bg-green-600' : 'bg-blue-600'}`}>
-                {isTag?(
+            <div className={`h-9 ${props.isTag ? 'bg-green-600' : 'bg-blue-600'}`}>
+                {props.isTag?(
                     <h1 className='flex justify-center text-[13px] p-1'>каранханка</h1>
                 ):(<Slider/>)}
             </div>
           </>
         )}
       </div>
-    </div>
+    </motion.div>
   );
 };
 
--- src/Component/Foreground.js
+++ src/Component/Foreground.js
@@ -1,21 +1,23 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Cards from './Cards';
 
+
 const Foreground = () => {
+    const ref = useRef(null);
   return (
-    <div className='fixed z-20 h-screen w-full top-0 left-0 bg-black opacity-75'> 
+    <div ref = {ref} className='fixed z-20 h-screen w-full top-0 left-0 bg-black bg-opacity-75'> 
         <div className='flex flex-wrap gap-x-80 p-2'>
-            <Cards/>
-            <Cards/>
+            <Cards drag showFooter={true} isTag={true} reference = {ref}/>
+            <Cards showFooter={false} isColor={true} isTag={true} reference = {ref}/>
         </div>
-        <div className='flex flex-wrap gap-48'>
+        <div className='flex flex-wrap gap-48 p-2'>
             <div className='flex flex-wrap gap-x-4'>
-            <Cards/>
-            <Cards/>
-            <Cards/>
+                <Cards showFooter={true} isTag={true} />
+                <Cards showFooter={false} isColor={false} isTag={false} reference = {ref}/>
+                <Cards showFooter={false} isColor={false} isTag={false} reference = {ref}/>
             </div>
             <div>
-                <Cards/>
+                <Cards showFooter={true} isTag={true} reference = {ref}/>
             </div>
         </div>
     </div>
