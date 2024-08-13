694c17210e89374f49f85b22834a24fb6b48babc
Karan Khanka <karankhanka@icloud.com>
2024-08-13 13:04:58 +0530

    cards

--- src/Component/Cards.js
+++ src/Component/Cards.js
@@ -3,25 +3,38 @@ import DescriptionIcon from '@mui/icons-material/Description';
 import DownloadForOfflineIcon from '@mui/icons-material/DownloadForOffline';
 
 const Cards = () => {
-    const showFooter = false;
+  const showFooter = false;
+  const isColor = true;
+  const isTag = false;
+
   return (
     <div className='relative w-48 h-60 bg-zinc-900/90 rounded-[30px] top-16 left-7 p-4 overflow-hidden'>
-      <DescriptionIcon className='absolute text-white text-3xl top-5 left-3'  />
-      <h1 className='text-white text-[13px] mt-10'> 
-        После установки пакетов вы можете импортировать значки, которые хотите 
+      <DescriptionIcon className='absolute text-white text-3xl top-5 left-3' />
+      <h1 className='text-white text-[13px] mt-10'>
+        После установки пакетов вы можете импортировать значки, которые хотите
       </h1>
-      <div className='absolute bottom-0 w-full'>
+      <div className='absolute bottom-0 w-full left-0'>
         {showFooter ? (
-          <div className='h-9 w-full bg-sky-50'></div>
-        ) : (
-          <div className='flex justify-between items-center bg-zinc-900/90 pr-9 pb-6'>
-            <h1 className='text-white text-[14px]'>5mb</h1>
-            <DownloadForOfflineIcon className='text-white' />
+          <div className={`flex justify-between items-center bg-zinc-900/90 pr-7 pb-6 pl-4 ${isColor ? 'text-white' : 'text-white'}`}>
+            <h1 className='text-[14px] '>
+              5mb
+            </h1>
+            <DownloadForOfflineIcon/>
           </div>
+        ) : (
+          <>
+            <div className={`flex justify-between items-center bg-zinc-900/90 pr-7 pb-6 pl-4 ${isColor ? 'text-green-500' : 'text-blue-500'}`}>
+            <h1 className={`text-[14px] ${isColor ? 'text-green-500' : 'text-blue-500'}`}>
+              5mb
+            </h1>
+              <DownloadForOfflineIcon className='text-white'/>
+            </div>
+            <div className={`h-9 ${isTag ? 'bg-green-600' : 'bg-blue-600'}`}></div>
+          </>
         )}
       </div>
     </div>
   );
-}
+};
 
 export default Cards;
