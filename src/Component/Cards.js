7982b15d0a5cac7b52645447d3fd592248eb0fa4
Karan Khanka <karankhanka@icloud.com>
2024-08-13 11:00:48 +0530

    logic

--- src/Component/Cards.js
+++ src/Component/Cards.js
@@ -3,18 +3,22 @@ import DescriptionIcon from '@mui/icons-material/Description';
 import DownloadForOfflineIcon from '@mui/icons-material/DownloadForOffline';
 
 const Cards = () => {
+    const showFooter = false;
   return (
     <div className='relative w-48 h-60 bg-zinc-900/90 rounded-[30px] top-16 left-7 p-4 overflow-hidden'>
       <DescriptionIcon className='absolute text-white text-3xl top-5 left-3'  />
       <h1 className='text-white text-[13px] mt-10'> 
         После установки пакетов вы можете импортировать значки, которые хотите 
       </h1>
-      <div className='flex justify-between text-white '>
-        <h1>5mb</h1>
-        <DownloadForOfflineIcon/>
-      </div>
-      <div className='absolute h-9 w-full bg-sky-50 bottom-0 left-0'>
-
+      <div className='absolute bottom-0 w-full'>
+        {showFooter ? (
+          <div className='h-9 w-full bg-sky-50'></div>
+        ) : (
+          <div className='flex justify-between items-center bg-zinc-900/90 pr-9 pb-6'>
+            <h1 className='text-white text-[14px]'>5mb</h1>
+            <DownloadForOfflineIcon className='text-white' />
+          </div>
+        )}
       </div>
     </div>
   );
